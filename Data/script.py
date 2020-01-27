90c8fca7a07adc7d1bb6fd2db209076950cd2e5b
Izentar <37346098+Izentar@users.noreply.github.com>
2020-01-27 18:32:59 +0100

    Zmodyfikowano skrypt python, aby pokazywal blad na zbiorze testowym.

--- Data/script.py
+++ Data/script.py
@@ -8,8 +8,9 @@ from scipy.sparse import csc_matrix
 Simple comment
 """
 
-# use this to divide
+# control variables
 divideCenter = 201
+numberOfTrees = 20
 
 matrix = np.loadtxt("newClevelandNoAmount.txt", usecols=range(14), dtype=int)
 matrixLower = matrix[:divideCenter, :14]
@@ -18,6 +19,8 @@ matrixUpper = matrix[divideCenter:, :14]
 matrixLowerTiny = np.delete(matrixLower, 13, 1)
 matrixUpperTiny = np.delete(matrixUpper, 13, 1)
 
+matrixLowerDiagnoseCol = matrixLower[:,13]
+matrixUpperDiagnoseCol = matrixUpper[:,13]
 
 """
 print(matrix)
@@ -31,7 +34,7 @@ print(matrixUpper)
 diagnoseColumn = matrix[:,13]
 matrix = np.delete(matrix, 13, 1)
 
-clf = AdaBoostClassifier(n_estimators=50, random_state=0)
+clf = AdaBoostClassifier(n_estimators=numberOfTrees, random_state=0)
 
 print(clf.fit(matrix, diagnoseColumn))  # train
 print(clf.predict([[61, 1, 4, 138, 166, 0, 2, 125, 1, 36, 2, 1, 3]])) #1
@@ -44,21 +47,24 @@ print("\n------------------------------------------------------------\n")
 sumTrue = 0
 sumFalse = 0
 
+'''
 for i in range(np.size(matrixUpper, 0)):
+    
     x = matrixUpper[i][13]
     print('Real data: ', x, ' Calculated data:', end=' ', sep='')
     tmpData = matrixUpperTiny[i][:13]
-    #print(tmpData)
     y = clf.predict([tmpData])
     print(y, 'equal resoults: ', x == y, sep=' ')
     if x == y:
         sumTrue+=1
     else:
         sumFalse+=1
-        
+    
 print('\nNumber of False:', sumFalse, ' Number of True:', sumTrue, sep=' ')  
+'''
+    
 
-print('Algorithm score:', clf.score(matrix, diagnoseColumn))
+print('Algorithm error:', 1 - clf.score(matrixUpperTiny, matrixUpperDiagnoseCol))
 
 
 
