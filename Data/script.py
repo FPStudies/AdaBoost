bf2768ecf9a477ba9aaf0f4be6b17933c31d0105
Izentar <37346098+Izentar@users.noreply.github.com>
2020-01-27 14:21:58 +0100

    Poprawiony skrypt.

--- Data/script.py
+++ Data/script.py
@@ -15,6 +15,10 @@ matrix = np.loadtxt("newClevelandNoAmount.txt", usecols=range(14), dtype=int)
 matrixLower = matrix[:divideCenter, :14]
 matrixUpper = matrix[divideCenter:, :14]
 
+matrixLowerTiny = np.delete(matrixLower, 13, 1)
+matrixUpperTiny = np.delete(matrixUpper, 13, 1)
+
+
 """
 print(matrix)
 print(type(matrix))
@@ -29,20 +33,32 @@ matrix = np.delete(matrix, 13, 1)
 
 clf = AdaBoostClassifier(n_estimators=50, random_state=0)
 
-print(clf.fit(matrix, diagnoseColumn))
+print(clf.fit(matrix, diagnoseColumn))  # train
 print(clf.predict([[61, 1, 4, 138, 166, 0, 2, 125, 1, 36, 2, 1, 3]])) #1
 print(clf.predict([[61, 1, 4, 140, 207, 0, 2, 138, 1, 19, 1, 1, 7]])) #1
 print(clf.predict([[47, 1, 3, 130, 253, 0, 0, 179, 0, 0, 1, 0, 3]])) #0
-print("------------------------------------------------------------\n")
+print("\n------------------------------------------------------------\n")
 
 #   np.size(matrixUpper, 0)
 
-for i in range(np.size(matrixUpper, 0)):
-    tmpData = matrixUpper[i][:13]
-    #print(tmpData)
-    print(clf.predict([tmpData]))
+sumTrue = 0
+sumFalse = 0
 
-print(clf.score(matrix, diagnoseColumn))
+for i in range(np.size(matrixUpper, 0)):
+    x = matrixUpper[i][13]
+    print('Real data: ', x, ' Calculated data:', end=' ', sep='')
+    tmpData = matrixUpperTiny[i][:13]
+    #print(tmpData)
+    y = clf.predict([tmpData])
+    print(y, 'equal resoults: ', x == y, sep=' ')
+    if x == y:
+        sumTrue+=1
+    else:
+        sumFalse+=1
+        
+print('\nNumber of False:', sumFalse, ' Number of True:', sumTrue, sep=' ')  
+
+print('Algorithm score:', clf.score(matrix, diagnoseColumn))
 
 
 
